5c0413f277080cf9b43cc710e9d0276a12085da8
Jhennerson Barbosa da Silva <92925449+jhennerson@users.noreply.github.com>
2023-07-11 23:12:59 -0300

    GET by list

--- dslist/src/main/java/com/devsuperior/dslist/dto/GameListDTO.java
+++ dslist/src/main/java/com/devsuperior/dslist/dto/GameListDTO.java
@@ -18,17 +18,7 @@ public class GameListDTO {
 		return id;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
-
 	public String getName() {
 		return name;
 	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	
 }
--- dslist/src/main/java/com/devsuperior/dslist/services/GameService.java
+++ dslist/src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepository;
 
 @Service //@Component
@@ -18,13 +20,20 @@ public class GameService {
 	private GameRepository gameRepository;
 	
 	@Transactional(readOnly = true) //não irá bloquear o banco de dados para escrita
-	public GameDTO findById(Long id) {
+	public GameDTO findById(@PathVariable Long id) {
 		Game result = gameRepository.findById(id).get();		
 		return new GameDTO(result);
 	}
-
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
-		return result.stream().map(x -> new GameMinDTO(x)).toList(); //transforma uma list de games em uma list de DTO
+		return result.stream().map(x -> new GameMinDTO(x)).toList(); //transforma uma list de GameMinProjection em uma list de GameMinDTO
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId) {
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
+		return result.stream().map(x -> new GameMinDTO(x)).toList(); //transforma uma list de GameMinProjection em uma list de GameMinDTO
 	}
 }
