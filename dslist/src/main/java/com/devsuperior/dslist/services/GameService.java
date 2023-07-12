a19a4fdf320b527b888c91b95e70cb4dea5a99a0
Jhennerson Barbosa da Silva <92925449+jhennerson@users.noreply.github.com>
2023-07-11 21:40:00 -0300

    GET lists

--- dslist/src/main/java/com/devsuperior/dslist/services/GameService.java
+++ dslist/src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
@@ -15,6 +17,12 @@ public class GameService {
 	@Autowired //injetando uma instância de GameRepository no GameService
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true) //não irá bloquear o banco de dados para escrita
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();		
+		return new GameDTO(result);
+	}
+
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList(); //transforma uma list de games em uma list de DTO
